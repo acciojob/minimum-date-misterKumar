3449c1009ad3f716e07ddd93f50254e2dd56f4e2
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-02 06:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 function minDate(dates) {
-  //write you code here
+	
+    let minYear = 9999;
+  	
+	for (let x of dates) {
+		let year = Number(x.slice(0 , 4));
+		minYear = Math.min(year , minYear);
+	}
+
+	console.log(minYear);
+
+	dates = dates.filter((ele) => {
+		let year = Number(ele.slice(0 , 4));
+		if(year == minYear)return ele;
+	});
+
+
+	
+	let minMonth = 13;
+
+	for(let x of dates){
+	
+		let month = Number(x.slice(5 , 7));
+		minMonth = Math.min(month , minMonth);
+	}
+
+	dates = dates.filter((ele) => {
+		let month = Number(ele.slice(5 , 7));
+		if(month == minMonth)return ele;
+	});
+
+	let minDay = 32;
+	for(let x of dates){
+		minDay = Math.min(Number(x.slice(8)) , minDay);
+	}
+
+	for(let x of dates){
+		let day = Number(x.slice(8));
+		if(day == minDay)return x;
+	}
 }
 
 // Do not change the code
